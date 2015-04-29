8918d42743d75b463a3bd6bcdc5cdfd2edb69eff
Maurits van Rees <maurits@vanrees.org>
2015-04-29 16:26:17 +0200

    Add test for checking the Modify permission per item.

--- collective/searchandreplace/tests/test_replacewhere.py
+++ collective/searchandreplace/tests/test_replacewhere.py
@@ -20,7 +20,8 @@
 __author__ = '''Brent Lambert, David Ray, Jon Thomas'''
 __version__ = '$ Revision 0.0 $'[11:-2]
 
-from base import SearchAndReplaceTestCase
+from Products.CMFPlone.tests.PloneTestCase import default_user
+from collective.searchandreplace.tests.base import SearchAndReplaceTestCase
 from zope.component import getUtility
 from collective.searchandreplace.interfaces import ISearchReplaceUtility
 
@@ -72,6 +73,55 @@ class testReplaceWhere(SearchAndReplaceTestCase):
             replaceText='foo',
             matchCase=False)
         self.assertEqual(len(results), 1)
+
+    def testReplaceOnlyEditableContent(self):
+        self.loginAsPortalOwner()
+        # /mainfolder
+        self.portal.invokeFactory('Folder', 'mainfolder')
+        mainfolder = getattr(self.portal, 'mainfolder')
+        mainfolder.setTitle('Test Title')
+
+        # /mainfolder/maindoc
+        mainfolder.invokeFactory('Document', 'maindoc')
+        maindoc = getattr(mainfolder, 'maindoc')
+        maindoc.setTitle('Test Title')
+
+        # /mainfolder/subfolder
+        mainfolder.invokeFactory('Folder', 'subfolder')
+        subfolder = getattr(mainfolder, 'subfolder')
+        subfolder.setTitle('Test Title')
+
+        # /mainfolder/subfolder/subdoc
+        subfolder.invokeFactory('Document', 'subdoc')
+        subdoc = getattr(subfolder, 'subdoc')
+        subdoc.setTitle('Test Title')
+
+        # Give test user a local Editor role on the sub folder.
+        subfolder.manage_addLocalRoles(default_user, ('Editor',))
+
+        # We are logged in as portal owner, so we can edit everything.
+        results = self.srutil.searchObjects(
+            mainfolder,
+            'test title',
+            replaceText='foo',
+            matchCase=False)
+        self.assertEqual(len(results), 4)
+
+        # Login as the test user again.
+        self.logout()
+        self.login()
+        # Now we can edit less: only the sub folder and sub doc.
+        results = self.srutil.searchObjects(
+            mainfolder,
+            'test title',
+            replaceText='foo',
+            matchCase=False)
+        self.assertEqual(len(results), 2)
+        paths = [x['path'] for x in results]
+        self.assertIn('/'.join(subfolder.getPhysicalPath()), paths)
+        self.assertIn('/'.join(subdoc.getPhysicalPath()), paths)
+        self.assertNotIn('/'.join(mainfolder.getPhysicalPath()), paths)
+        self.assertNotIn('/'.join(maindoc.getPhysicalPath()), paths)
 
 
 def test_suite():
