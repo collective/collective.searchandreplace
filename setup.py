1aa15b013f1e4eb5ec068e0cccfa530bb716df3d
Maurits van Rees <maurits@vanrees.org>
2015-10-27 17:45:35 +0100

    Claimed Plone 5 compatibility in setup.py.

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(name='collective.searchandreplace',
       classifiers=[
           "Framework :: Plone",
           "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.0",
           "Programming Language :: Python",
           "Programming Language :: Python :: 2.7",
           "Topic :: Software Development :: Libraries :: Python Modules",
