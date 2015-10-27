601eaaccf5dd6d94087c0cf9ce96208aa3705b20
Maurits van Rees <maurits@vanrees.org>
2015-10-27 19:32:25 +0100

    Back to development: 5.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ __version__ = '$ Revision 0.0 $'[11:-2]
 
 from setuptools import setup, find_packages
 
-version = '5.1'
+version = '5.2.dev0'
 
 setup(name='collective.searchandreplace',
       version=version,
