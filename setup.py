2d5e980d5728a674e36712045608465659e5c8a7
Maurits van Rees <maurits@vanrees.org>
2016-02-15 10:32:01 +0100

    Back to development: 6.0.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ __version__ = '$ Revision 0.0 $'[11:-2]
 
 from setuptools import setup, find_packages
 
-version = '6.0.3'
+version = '6.0.4.dev0'
 
 setup(name='collective.searchandreplace',
       version=version,
