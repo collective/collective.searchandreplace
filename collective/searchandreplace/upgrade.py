61fcdd7a381eed1d23ceaf21318e2ec942468514
Maurits van Rees <maurits@vanrees.org>
2016-07-18 15:43:48 +0200

    Revert "Added reindex_object_provides upgrade function."
    This reverts commit ee5984cda200fb410bf5495bc66f358183e12279.

--- collective/searchandreplace/upgrade.py
+++ collective/searchandreplace/upgrade.py
@@ -1,9 +1,3 @@
-from Products.CMFCore.utils import getToolByName
-
-import logging
-
-
-logger = logging.getLogger('collective.searchandreplace')
 PROFILE_ID = 'profile-collective.searchandreplace:default'
 
 
@@ -21,29 +15,3 @@ def run_rolemap_step(context):
 
 def run_registry_step(context):
     context.runImportStepFromProfile(PROFILE_ID, 'plone.app.registry')
-
-
-def reindex_object_provides(context, portal_type=None):
-    """Reindex the object_provides index.
-
-    This is needed when more or less portal_types provide our interface.
-
-    portal_type can be:
-
-    - None (default): reindex the index for the whole site.
-
-    - a string or a list of strings: get the brains of these
-    portal_types, and reindex the index only for those objects.
-    """
-    catalog = getToolByName(context, 'portal_catalog')
-    index = 'object_provides'
-    if portal_type is None:
-        logger.info('Reindexing %s in whole site.', index)
-        catalog.manage_reindexIndex(ids=[index])
-        return
-    brains = catalog.unrestrictedSearchResults(portal_type=portal_type)
-    logger.info('Reindexing %s for %d items of these portal_types: %r.',
-                index, len(brains), portal_type)
-    for brain in brains:
-        obj = brain.getObject()
-        obj.reindexObject(idxs=[index])
