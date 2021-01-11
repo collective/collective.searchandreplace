8d10e8a8fc120739322bfe3e224ac880f0290c87
Maurits van Rees <maurits@vanrees.org>
2021-01-11 14:24:18 +0100

    Added more setup.py classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -27,13 +27,14 @@ setup(
     version=version,
     description="Batch Search and Replace",
     long_description=(open("README.rst").read() + "\n" + open("CHANGES.rst").read()),
-    # Get more strings from
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    # Get more strings from https://pypi.org/classifiers/
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.7",
