73320cb5b8a1bd5e53d7723e178d82e2206b094f
Godefroid Chapelle <gotcha@bubblenet.be>
2023-06-27 16:48:35 +0200

    add a nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      ( system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          my-python-packages = python-packages: with python-packages; [
+            virtualenv
+          ];
+          python-with-my-packages = pkgs.python311.withPackages my-python-packages;
+        in
+        with pkgs;
+        { devShells.default = pkgs.mkShell {
+            packages = [
+              pkg-config
+              python-with-my-packages
+              libxml2
+              libxslt
+            ];
+          };
+        }
+      );
+}
