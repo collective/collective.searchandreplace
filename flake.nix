2386873e30f2c6be71dff7a759f16b4e44d57b1f
Godefroid Chapelle <gotcha@bubblenet.be>
2023-10-05 20:11:45 +0200

    more test oauth

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils"; # loop over systems
+    flake-utils.url = "github:numtide/flake-utils"; # loop
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem
